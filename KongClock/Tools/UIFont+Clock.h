8a487499bcd925c62e51bbcabd092bfea498503f
kongfanwu <kongfanwu@zuoyebang.com>
2024-01-26 19:15:39 +0800

    feature：字体颜色参数支持

---
+++ KongClock/Tools/UIFont+Clock.h
@@ -0,0 +1,16 @@
+//
+//  UIFont+Clock.h
+//  KongClock
+//
+//  Created by K on 2024/6/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIFont (Clock)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KongClock/Tools/UIFont+Clock.m
@@ -0,0 +1,12 @@
+//
+//  UIFont+Clock.m
+//  KongClock
+//
+//  Created by K on 2024/6/27.
+//
+
+#import "UIFont+Clock.h"
+
+@implementation UIFont (Clock)
+
+@end
